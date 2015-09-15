20ed350f878e0dbf2a2f9414f387613eba8f7b5d
Lars Thorup <lars@zealake.com>
2015-09-15 15:18:22 +0200

    initial scenario test step

---
+++ src/client.js
@@ -0,0 +1,15 @@
+'use strict';
+
+class Client {
+  constructor (name) {
+
+  }
+
+  ranking (statement, rank) {
+    return Promise.resolve();
+  }
+}
+
+module.exports = {
+  Client: Client
+};
--- test/scenario.test.js
+++ test/scenario.test.js
@@ -1,6 +1,18 @@
 /* eslint-env amd, mocha */
+
+var Client = require('../src/client').Client;
+
+// Note: being a scenario test, the individual it() blocks are not meant to be run in isolation
 describe('scenario', () => {
-  it('works', () => {
-    (2 + 2).should.equal(4);
+  before(() => {
+    this.clientSusan = new Client('clientSusan');
   });
+
+  it('should send data upstream', () => {
+    return this.clientSusan.ranking('statementPeace', 4);
+  });
+
+  it('should receive data from downstream');
+
+  it('should sync data downstream to interested parties');
 });
